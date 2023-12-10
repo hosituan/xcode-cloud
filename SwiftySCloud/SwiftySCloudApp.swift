9718ad1b8e9a2402bc1ce5a1445ee27abb63b5bc
hosituan <hosituan.work@gmail.com>
2023-12-10 20:55:00 +0700

    Initial Commit

---
+++ SwiftySCloud/SwiftySCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySCloudApp.swift
+//  SwiftySCloud
+//
+//  Created by Ho Si Tuan on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
